07d061d38508d6f82c24b95922f472b4f07f4711
Junayed5 <mdjunayed352003@gmail.com>
2024-04-01 23:26:38 +0600

    created products section

---
+++ src/layout/Products.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Products = () => {
+    return (
+        <div className='my-20'>
+            <h2 className='font-semibold text-3xl text-center'>KIND KLASSIKS</h2>
+            <p className='border-b-2 w-36 mx-auto border-black py-1'></p>
+        </div>
+    );
+};
+
+export default Products;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Slider from "../components/Slider/Slider";
 import Search from "../components/Search";
+import Products from "../layout/Products";
 
 const Home = () => {
   return (
     <>
       <Slider />
+      <Products/>
     </>
   );
 };
