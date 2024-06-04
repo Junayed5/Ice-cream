fd1d9b25590270a5103f094b07c3d4c31fb4763c
Junayed5 <mdjunayed352003@gmail.com>
2024-06-04 12:49:12 +0600

    fotter complete

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -8,7 +8,7 @@ import { TbClockHour8 } from "react-icons/tb";
 
 const Footer = () => {
   return (
-    <div className="h-[755px] bg-[#D37677]">
+    <div className="h-[755px] w-full bg-[#D37677]">
       <div className="w-fit mx-auto">
         <img src="tittle.png" alt="" />
         <p className="text-white text-xl font-semibold text-center mt-6 mb-3">
@@ -28,12 +28,12 @@ const Footer = () => {
         <FaWhatsapp className="h-7 w-7" />
       </div>
 
-      <div className="grid grid-cols-3 gap-5">
-        <div className=" w-[675px]">
+      <div className="grid grid-cols-3 gap-10 my-14 ml-60">
+        <div className="pt ">
           <h3 className="text-xl text-white font-semibold border-gray-300 border-b pb-4">
             SINGAPORE
           </h3>
-          <div className="grid grid-cols-2 gap-y-5">
+          <div className="grid grid-cols-2 gap-y-8 gap-x-10 pt-10">
             <div className="text-white">
               <h4 className="text-xl border-b w-fit">Forum Shopping Mall</h4>
               <p className="flex items-center">
@@ -96,11 +96,11 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className=" w-[415px]">
+        <div className="pt">
           <h3 className="text-xl text-white font-semibold border-gray-300 border-b pb-4">
-            SINGAPORE
+            MALAYSIA
           </h3>
-          <div className="grid grid-rows-2 gap-y-5">
+          <div className="grid grid-rows-2 gap-y-5 pt-10">
             <div className="text-white">
               <h4 className="text-xl border-b w-fit">Forum Shopping Mall</h4>
               <p className="flex items-center">
@@ -130,6 +130,34 @@ const Footer = () => {
               <p className="flex items-center">
                 <BsFillTelephoneFill /> &nbsp; 90108557
               </p>
+            </div>
+          </div>
+        </div>
+        <div className=" border-gray-300 border-l pl-8">
+          <h3 className="text-xl text-white font-semibold pb-4 ">INQUIRY</h3>
+
+          <div className="text-white">
+            <h4 className="text-xl w-fit">Singapore</h4>
+            <div className="py-3 pl-5">
+              <p className="flex items-center">+65 91528855</p>
+              <p className="flex items-center">+65 91528855</p>
+            </div>
+          </div>
+          <div className="text-white">
+            <h4 className="text-xl w-fit">Malaysia</h4>
+            <div className="py-3 pl-5">
+              <p className="flex items-center">+65 91528855</p>
+              <p className="flex items-center">+65 91528855</p>
+            </div>
+          </div>
+          <div className="text-white">
+            <h4 className="text-2xl font-bold w-fit">SUPPORT</h4>
+            <div className="py-3 pl-5">
+              <p className="flex items-center">Terms of Service</p>
+              <p className="flex items-center">Privacy Policy</p>
+              <p className="flex items-center">Refund Policy</p>
+              <p className="flex items-center">Shipping Policy</p>
+              <p className="flex items-center">Loyalty Program</p>
             </div>
           </div>
         </div>
